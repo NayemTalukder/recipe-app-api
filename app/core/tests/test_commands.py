005110fcdf89eb24e3edd0a8a6535fc5c8b798dc
NayemTalukder <nayemtalukder999@gmail.com>
2023-01-09 15:50:01 +0600

    handle method completed in wait_for_db | noqa added | databases spelling mistake solved

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -20,7 +20,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -32,4 +32,4 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
