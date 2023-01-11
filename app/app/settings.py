b88ebdc4862b606774583cddecc501fd5c65bb96
NayemTalukder <nayemtalukder999@gmail.com>
2023-01-11 18:37:04 +0600

    Updated requirements.txt to include drf-spectacular | Added drf_spectacular to settings | Added config for drf-spectacular

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
     'core',
 ]
 
@@ -135,3 +136,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular_openapi.AutoSchema'
+}
