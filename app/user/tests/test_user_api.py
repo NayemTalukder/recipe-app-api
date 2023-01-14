0c14fff26cffcf44917ec4c6acb1e542f299c564
NayemTalukder <nayemtalukder999@gmail.com>
2023-01-14 15:29:35 +0600

    Implemented user API.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -37,14 +37,16 @@ class PublicUserApiTesta(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
 
-    def test_user_with_email_exista_error(self):
-        """Test error returned if user with email exista."""
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists."""
         payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
             'name': 'test Name',
         }
         create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -55,7 +57,7 @@ class PublicUserApiTesta(TestCase):
             'password': 'pw',
             'name': 'Test name'
         }
-        res = self.assertEqual(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
