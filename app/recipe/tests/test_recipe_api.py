9ae52b51550cea6ce5f186dc8087d97d779d46a3
NayemTalukder <nayemtalukder999@gmail.com>
2023-01-21 14:40:51 +0600

    Add create recipe API.

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -111,7 +111,7 @@ class PrivateRecipeTests(TestCase):
         }
         res = self.client.post(RECIPES_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_201)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         recipe = Recipe.objects.get(id=res.data['id'])
         for k, v in payload.items():
             self.assertEqual(getattr(recipe, k), v)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -25,3 +25,7 @@ class Recipevieset(viewsets.ModelViewSet):
         if self.action == 'list':
             return serializers.RecipeSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
