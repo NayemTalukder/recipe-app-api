19b8cf12eada6c076d660409c2ed2d1081aebbe6
NayemTalukder <nayemtalukder999@gmail.com>
2023-01-27 21:03:11 +0600

    Tests for uploading images

--- app/core/models.py
+++ app/core/models.py
@@ -69,7 +69,7 @@ class Recipe(models.Model):
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
     ingredients = models.ManyToManyField('Ingredient')
-    Image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
 
     def __str__(self):
         return self.title
